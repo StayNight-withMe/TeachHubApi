c870ea5ea011897f5d6211a938747d5934d91ee5
Виктор <kasa.king_over@mail.ru>
2025-12-29 01:05:40 +0300

    refact

---
+++ Application/Abstractions/Service/ICourseService.cs
@@ -0,0 +1,53 @@
+﻿using Core.Model.ReturnEntity;
+using Core.Model.TargetDTO.Common.input;
+using Core.Model.TargetDTO.Common.output;
+using Core.Model.TargetDTO.Courses.input;
+using Core.Model.TargetDTO.Courses.output;
+using Core.Model.TargetDTO.LessonFile.input;
+using System.Security.Claims;
+
+namespace Core.Interfaces.Service
+{
+    public interface ICourseService
+    {
+        public Task<TResult> CreateCourse(
+            CreateCourseDTO courceDTO, 
+            int id, 
+            CancellationToken ct = default);
+        public Task<TResult<PagedResponseDTO<CourseOutputDTO>>> GetAllCourse(
+            SortingAndPaginationDTO userSortingRequest,
+            CancellationToken ct = default);
+        public Task<TResult<PagedResponseDTO<CourseOutputDTO>>> GetUserCourses(
+            int id, 
+            string name, 
+            SortingAndPaginationDTO userSortingRequest,
+            CancellationToken ct = default
+            );
+        public Task<TResult<CourseOutputDTO>> UpdateCourse(
+            UpdateCourseDTO updateCourseDTO, 
+            int userid,
+            CancellationToken ct = default);
+        public Task<TResult<PagedResponseDTO<CourseOutputDTO>>> SearchCourse(
+            string search, 
+            SortingAndPaginationDTO userSortingRequest,
+            int userid = default,
+            CancellationToken ct = default
+            );
+
+        public Task<TResult> RemoveCourse(
+            int id, 
+            ClaimsPrincipal user, 
+            CancellationToken ct = default);
+
+        Task<TResult> SetImgFile(
+            Stream stream,
+            int userid,
+            CourseSetImageDTO courseSetImageDTO,  
+            string contentType,
+            CancellationToken ct = default
+              );
+
+
+
+    }
+}
---
+++ Application/Abstractions/Service/IReviewService.cs
@@ -0,0 +1,46 @@
+﻿using Core.Model.ReturnEntity;
+using Core.Model.TargetDTO.Common.input;
+using Core.Model.TargetDTO.Common.output;
+using Core.Model.TargetDTO.Review.input;
+using Core.Model.TargetDTO.Review.output;
+
+
+namespace Core.Interfaces.Service
+{
+    public interface IReviewService
+    {
+        Task<TResult> PostReview(
+            ReviewICreateDTO review, 
+            int userid,
+            CancellationToken ct = default
+            );
+        Task<TResult<PagedResponseDTO<ReviewOutputDTO>>> GetReviewsByCourseId(
+            int courseId,
+            SortingAndPaginationDTO sortingAndPagination,
+            CancellationToken ct = default
+            );
+
+        Task<TResult<PagedResponseDTO<ReviewOutputDTO>>> GetReviewsByUserId(
+            int userid,
+            SortingAndPaginationDTO sortingAndPagination,
+            CancellationToken ct = default
+            );
+
+        Task<TResult<ReviewOutputDTO>> UpdateReview(
+            ReviewChangedDTO changedDTO, 
+            int userid,
+            CancellationToken ct = default
+            );
+
+
+        Task<TResult> DeleteReview(
+            int reviewId,
+            int userId,
+            CancellationToken ct = default
+            );
+
+        
+
+
+    }
+}
