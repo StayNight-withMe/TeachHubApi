d43900578065d04e1c7e6ee4d06ce34c40280c3e
Виктор <kasa.king_over@mail.ru>
2026-01-21 17:50:33 +0300

    refactor: delete unused links

--- Logger/LoggerMessage.cs
+++ Logger/LoggerMessage.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Logger
 {
