4a3f4a88c58419834f0fbeba0d3a36d301f4c228
Виктор <kasa.king_over@mail.ru>
2026-01-28 11:51:18 +0300

    feat: create profile controller

--- testApi/EndPoints/ProfileController.cs
+++ testApi/EndPoints/ProfileController.cs
@@ -1,6 +1,33 @@
-﻿namespace testApi.EndPoints
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testApi.EndPoints
 {
-    public class ProfileController
+    
+    [ApiController]
+    [Route("api/profile")]
+    [Tags("Профили")]
+    [ApiVersion("1.0")]
+    public class ProfileController : ControllerBase
     {
+        public Task<IActionResult> UpdateProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ChangeProfileIcon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetUserProfile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetMyProfile()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
