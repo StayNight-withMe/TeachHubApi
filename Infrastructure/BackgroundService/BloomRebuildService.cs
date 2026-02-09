fcdaecc4a42972d46ddb0be2381776de4e5453b6
Виктор <kasa.king_over@mail.ru>
2026-02-09 23:57:38 +0300

    style: space correct

--- Infrastructure/BackgroundService/BloomRebuildService.cs
+++ Infrastructure/BackgroundService/BloomRebuildService.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-
-
 namespace infrastructure.BackgroundService
 {
     public class BloomRebuildService : Microsoft.Extensions.Hosting.BackgroundService
